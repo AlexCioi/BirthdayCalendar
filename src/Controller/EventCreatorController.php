97e7e8c1b65d520499320d3a9b3de8931b75e1dd
AlexC2103 <alexandrucioi@gmail.com>
2023-03-01 01:08:07 +0200

    desktop

--- src/Controller/EventCreatorController.php
+++ src/Controller/EventCreatorController.php
@@ -2,17 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Event;
+use App\Form\EventCreateFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventCreatorController extends AbstractController
 {
-    #[Route('/event/creator', name: 'app_event_creator')]
-    public function index(): Response
+    #[Route('/dashboard/events/creator', name: 'app_event_creator')]
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $event = new Event();
+
+        $form = $this->createForm(EventCreateFormType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $event->setUser($this->getUser()->getUserIdentifier());
+
+            $formData = $form->getData();
+
+            $event->setName($formData->getName());
+            $event->setDueDate($formData->getDueDate());
+            $event->setDescription($formData->getDescription());
+
+            $entityManager->persist($event);
+            $entityManager->flush();
+
+            $event = new Event();
+        }
+
         return $this->render('event_creator/index.html.twig', [
             'controller_name' => 'EventCreatorController',
+            'form' => $form
         ]);
     }
 }
